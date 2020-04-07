f4b602a9d3356f88efa16c97c54ec4b639dfe96c
Ashley <Ashley.Barker@traineeacademy.com>
2020-04-07 16:33:24 +0100

    java week 1 files added

---
+++ src/com/qac/day1/Runner.java
@@ -0,0 +1,28 @@
+package com.qac.day1;
+
+public class Runner {
+
+	public static void main(String args[]) {
+
+		// this is a single line comment
+		/*
+		 * this is a multi-line comment
+		 */
+		Boolean bool = true;
+		String homeAdd = "1234 Example Road, London, M50 3SA";
+		int height = 176;
+		int age = 23; // data type being int, variable named age has a value of 23
+		System.out.println("Hello World");
+
+		System.out.println("I am " + age); // + means append variable
+
+		System.out.println("I am " + height + "cm");
+
+		System.out.println("My address is " + homeAdd);
+
+		System.out.println("The value of the set boolean is " + bool);
+
+		System.out.println("My height is " + height + " my address is " + homeAdd + " value of bool is " + bool);
+	}
+
+}
